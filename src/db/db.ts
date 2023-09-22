9e88aee4b94e565440e6b7ff85994ec682420eb7
Vaibhav <vaibhavpandeyprayag@gmail.com>
2023-09-23 02:07:11 +0530

    login and signup apis are working.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,19 +1,12 @@
 import { Pool } from "pg";
 require("dotenv").config();
 
-// const pool = new Pool({
-//   host: process.env.POSTGRESQL_ADDON_HOST,
-//   database: process.env.POSTGRESQL_ADDON_DB,
-//   user: process.env.POSTGRESQL_ADDON_USER,
-//   port: Number(process.env.POSTGRESQL_ADDON_PORT),
-//   password: process.env.POSTGRESQL_ADDON_PASSWORD,
-// });
 const pool = new Pool({
-  host: "localhost",
-  database: "ecommerce",
-  user: "postgres",
-  port: 5432,
-  password: process.env.LOCALPASS,
+  host: process.env.POSTGRESQL_ADDON_HOST,
+  database: process.env.POSTGRESQL_ADDON_DB,
+  user: process.env.POSTGRESQL_ADDON_USER,
+  port: Number(process.env.POSTGRESQL_ADDON_PORT),
+  password: process.env.POSTGRESQL_ADDON_PASSWORD,
 });
 
 export default pool;
--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -21,19 +21,30 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 authRouter.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const connection: PoolClient = await db.connect();
-
-  let query = `select password from user_auth_info where email = '${email}';`;
-  console.log("login query >>", query);
-  const response = await connection.query(query);
-  console.log(response);
-  if (response.rowCount > 0) {
-    const passwordFromDb = response.rows[0].password;
-    if (decrypt(password) === decrypt(passwordFromDb))
-      res.status(200).send({ status: 200, message: "Logged in successfully." });
-    else res.status(401).send({ status: 401, message: "Invalid credentials." });
-  } else res.status(401).send({ status: 401, message: "Invalid credentials." });
+  let connection: PoolClient | undefined = undefined;
+  try {
+    const { email, password } = req.body;
+    connection = await db.connect();
+    let query = `select password from user_auth_info where email = '${email}';`;
+    console.log("login query >>", query);
+    const response = await connection.query(query);
+    console.log(response);
+    if (response.rowCount > 0) {
+      const passwordFromDb = response.rows[0].password;
+      if (decrypt(password) === decrypt(passwordFromDb))
+        res
+          .status(200)
+          .send({ status: 200, message: "Logged in successfully." });
+      else
+        res.status(401).send({ status: 401, message: "Invalid credentials." });
+    } else
+      res.status(401).send({ status: 401, message: "Invalid credentials." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: 500, message: "Internal Server Error" });
+  } finally {
+    if (connection != undefined) connection.release();
+  }
 });
 
 export default authRouter;
